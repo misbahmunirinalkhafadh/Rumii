217eb65eb8540617258487e84f5f1fb8547cdbbb
misbahmunirinalkhafadh <alkhafadh.misbah10@gmail.com>
2021-07-04 18:50:14 +0700

    add: fonts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { StyleSheet, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FocusAwareStatusBar } from '../../hooks/useIsFocused';
-import { mdiAccount } from '@mdi/js'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const Home = () => {
     return (
         <SafeAreaView style={[styles.container, { backgroundColor: '#6a51ae' }]}>
             <FocusAwareStatusBar barStyle="light-content" backgroundColor="#6a51ae" />
             <Text>Home Screens</Text>
+            <MaterialCommunityIcons name="home" color="#000" size={23} />
+            <Ionicons name="ios-book" color="#000" size={23} />
+            <Feather name="book" color="#000" size={23} />
+            <FontAwesome5 name="book" color="#000" size={23} />
         </SafeAreaView>
     )
 }
